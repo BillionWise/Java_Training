924c4f9cd6106038ab80a617957a2e4fd3facf0e
Whalexzy <uthmanolawaleipadeola@gmail.com>
2025-03-21 14:03:26 +0000

    Facade Pattern

---
+++ 3-21-2025/src/FacadeExample.java
@@ -0,0 +1,23 @@
+class AudioPlayer {
+    void playAudio() {
+        System.out.println("Playing Audio");
+    }
+}
+
+
+class VideoPlayer
+{
+    void playVideo(){
+        System.out.println("Playing video");
+    }
+}
+
+class StreamingService{
+    void startStreaming(){
+        System.out.println("Starting streaming");
+    }
+}
+
+
+public class FacadeExample {
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main { 
   public static void main(String[] args) {
     System.out.println("Hello BillionWise");
     System.out.println("We are getting there");
