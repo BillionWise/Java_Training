af76288c01cd014186ade8d5ef3389e31d2d1550
Whalexzy <uthmanolawaleipadeola@gmail.com>
2025-03-21 15:17:51 +0000

    Facade Pattern

--- 3-21-2025/src/BankingSystem.java
+++ 3-21-2025/src/BankingSystem.java
@@ -1,2 +1,26 @@
+class Account {
+    void accountBalance() {
+        System.out.println("Account Balance");
+    }
+}
+
+
+class Security {
+    void verifyUser(){
+        System.out.println("Security Verify");
+    }
+}
+
+class TransactionLogger {
+    void logTransaction(Account account, Security security) {}
+}
+
+
+
+
+
+
+
+
 public class BankingSystem {
 }
